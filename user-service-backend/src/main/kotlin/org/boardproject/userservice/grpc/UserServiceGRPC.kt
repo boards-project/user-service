15fc5067115339927e311e8e54f3daffe6d74877
Eugene Karanda <eugene.karabda@gmail.com>
2018-11-04 13:31:55 +0200

    user-service-api has been updated to use reactive-grpc(grpc extension for project reactor) UserServiceGRPC has been refactored to handle it

--- user-service-backend/src/main/kotlin/org/boardproject/userservice/grpc/UserServiceGRPC.kt
+++ user-service-backend/src/main/kotlin/org/boardproject/userservice/grpc/UserServiceGRPC.kt
@@ -1,43 +1,35 @@
 package org.boardproject.userservice.grpc
 
-import io.grpc.stub.StreamObserver
 import org.boardproject.userservice.api.FindAllRequest
 import org.boardproject.userservice.api.FindOneRequest
+import org.boardproject.userservice.api.ReactorUserServiceGrpc
 import org.boardproject.userservice.api.UserReply
-import org.boardproject.userservice.api.UserServiceGrpc
 import org.boardproject.userservice.repository.UserRepository
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Service
-data class UserServiceGRPC(private val userRepository: UserRepository) : UserServiceGrpc.UserServiceImplBase() {
+data class UserServiceGRPC(private val userRepository: UserRepository) : ReactorUserServiceGrpc.UserServiceImplBase() {
 
-    override fun findAll(request: FindAllRequest, responseObserver: StreamObserver<UserReply>) {
-        userRepository.findAll()
+    override fun findAll(request: Mono<FindAllRequest>): Flux<UserReply> {
+        return userRepository.findAll()
                 .map {
                     UserReply.newBuilder()
                             .setId(it.id)
                             .setUsername(it.username)
                             .build()
                 }
-                .subscribe(
-                        responseObserver::onNext,
-                        responseObserver::onError,
-                        responseObserver::onCompleted
-                )
     }
 
-    override fun findOne(request: FindOneRequest, responseObserver: StreamObserver<UserReply>) {
-        userRepository.findById(request.id)
+    override fun findOne(request: Mono<FindOneRequest>): Mono<UserReply> {
+        return request.map { it.id }
+                .flatMap { userRepository.findById(it) }
                 .map {
                     UserReply.newBuilder()
                             .setId(it.id)
                             .setUsername(it.username)
                             .build()
                 }
-                .subscribe(
-                        responseObserver::onNext,
-                        responseObserver::onError,
-                        responseObserver::onCompleted
-                )
     }
 }
